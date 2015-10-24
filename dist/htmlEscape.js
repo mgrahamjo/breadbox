c998b1437852ab850b7c1d93dcbf0a85eaba5794
Mike <mike.johnson@booking.com>
2015-10-23 17:24:39 -0700

    add htmlEscape.reverse

--- dist/htmlEscape.js
+++ dist/htmlEscape.js
@@ -4,6 +4,12 @@ String.prototype.customReplaceAll = function (str1, str2, ignore) {
     return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|<\>\-\&])/g, "\\$&"), ignore ? "gi" : "g"), typeof str2 === "string" ? str2.replace(/\$/g, "$$$$") : str2);
 };
 
-module.exports = function (str) {
+function htmlEscape(str) {
     return String(str).customReplaceAll("&", "&amp;").customReplaceAll("<", "&lt;").customReplaceAll(">", "&gt;").customReplaceAll("\"", "&quot;").customReplaceAll("'", "&#39;");
+}
+
+htmlEscape.reverse = function (str) {
+    return String(str).customReplaceAll("&amp;", "&").customReplaceAll("&lt;", "<").customReplaceAll("&gt;", ">").customReplaceAll("&quot;", "\"").customReplaceAll("&#39;", "'");
 };
+
+module.exports = htmlEscape;
